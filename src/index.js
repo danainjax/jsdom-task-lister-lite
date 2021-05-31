2ad363abbaa1dadbf435a00af3afa92930bbf81f
danainjax <danainjax@gmail.com>
2021-05-31 10:24:23 -0400

    completed mvp deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//   // your code here
+// });
+const main = document.querySelector('#main-content');
+
+const taskForm = document.querySelector('#create-task-form');
+
+
+taskForm.addEventListener('submit', function(e) {
+  e.preventDefault()
+  const newTask = document.querySelector('#new-task-description').value;
+  console.log(newTask);
+  main.append(newTask);
+})
