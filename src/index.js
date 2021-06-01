bee8c71fe4b727aae71d8da87046daad4a071fa9
danainjax <danainjax@gmail.com>
2021-06-01 09:31:52 -0400

    followed along with evans wang video to learn how to add the delete button and action, coded along

--- src/index.js
+++ src/index.js
@@ -6,13 +6,49 @@ const main = document.querySelector('#main-content');
 const taskForm = document.querySelector('#create-task-form');
 
 const taskList = document.querySelector('#tasks');
+const theList = document.querySelector('#list');
+
 
 
 
 taskForm.addEventListener('submit', function(e) {
   e.preventDefault()
-  const newTask = document.querySelector('#new-task-description').value;
+  let newTask = document.querySelector('#new-task-description').value;
   newLi = document.createElement('li')
   newLi.innerText = newTask
-  taskList.append(newLi);
-})
+  
+  
+  
+ 
+  
+  taskList.innerHTML += `<li>${newTask}
+  <button data-action="delete"> delete </button> </li>`
+  
+  taskForm.reset();
+  console.log(theList);
+  
+  
+  
+    
+    
+       
+    
+  })
+  
+  taskList.addEventListener('click', function(e) {
+     
+    if (e.target.dataset.action === "delete") {
+      e.target.parentElement.remove()
+
+    }
+   
+
+  })
+
+
+
+
+
+// document.getElementById("myBtn").addEventListener("click", function() {
+//   document.getElementById("demo").innerHTML = "Hello World";
+// });
